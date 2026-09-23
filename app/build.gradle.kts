a9c38075df6e499a6a4891e28e10083b8532cdab
Carlos-Gan <gandarac052@gmail.com>
2026-09-23 13:00:18 -0600

    v1.6.3 -  Cambio de version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "dev.charlesmoran.flowschedule"
     minSdk = 26
     targetSdk = 37
-    versionCode = 1
-    versionName = "1"
+    versionCode = 2
+    versionName = "1.0.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
